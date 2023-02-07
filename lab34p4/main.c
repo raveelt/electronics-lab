3f305b5ca6f52771f837a8b30fd994622d7cacce
Raveel Tejani <t.raveel@gmail.com>
2023-02-07 15:13:18 -0800

    completed part 4 of lab manual

--- lab34p4/main.c
+++ lab34p4/main.c
@@ -11,19 +11,48 @@ void main(void)
     WDTCTL = WDTPW + WDTHOLD;
     P1DIR = 0b00000001;
     P4DIR = 0b10000000;
-    P1OUT = 0b00000011;
-    P4OUT = 0b10000000;
+    P1OUT = 0b00000010;
+    P4OUT = 0b00000000;
     P1REN = 0b00000010;
     P1IE = 0b00000010;
     P1IES= 0b00000010;
     _BIS_SR (LPM4_bits + GIE);
 
 
+
 }
+
+int n = 0;
 
 void __attribute__ ((interrupt(PORT1_VECTOR))) PORT1_ISR(void) {
 
-    P1OUT ^= 0b00000001;
-    P4OUT ^= 0b10000000;
+
+    _delay_cycles (500000);
+
+    if(n == 0){
+        P1OUT ^= 0b00000001;
+        n += 1;
+    }
+
+    else if(n == 1){
+
+        P1OUT ^= 0b00000001;
+        P4OUT ^= 0b10000000;
+        n += 1;
+    }
+
+    else if(n == 2){
+        P1OUT ^= 0b00000001;
+        n += 1;
+
+    }
+    else{
+        P1OUT ^= 0b00000001;
+        P4OUT ^= 0b10000000;
+        n = 0;
+
+    }
+
     P1IFG &= ~0b00000010;
+
 }
